4eae9c9debbfda3a25368a94deca3a0dbb6fafa0
Mitch Capper <mitch.capper@gmail.com>
2015-03-03 12:20:01 -0800

    Forgot to commit up the extension library files

---
+++ WPFDoistExtLib/iParserExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPFDoistExtLib
+{
+	public interface iParserExtension {
+		string regexp_to_find { get; } //regexp to find the initial link
+		string regexp_replace_with_func_body { get; }
+	}
+	public interface iParserExtensionLink : iParserExtension {
+		void HandleLink(String link);
+	}
+	public interface iParserExtensionProtocolHandler : iParserExtension {
+		string override_url_func_body { get; } //only required if $1 won't match the url for use
+	}
+
+}
